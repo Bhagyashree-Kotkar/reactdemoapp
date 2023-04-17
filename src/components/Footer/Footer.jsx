b54ce554c9530c1d2f587e0d267442effe1c8d16
Bhagyashree <bhagyashree.kotkar@ness.com>
2023-04-17 16:35:38 +0530

    reactdemocommit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        This is Footer component.
+      </footer>        
+    );
+  }
+}
+ 
+export default Footer;
